0890e22ecd5142e1ee2928feca1878a58eda5005
Lucien <lucienzhangzl@outlook.com>
2023-02-11 16:56:52 +0900

    fix path slipt

--- converter/main.py
+++ converter/main.py
@@ -16,11 +16,11 @@ TEMPLATE = JINJA_ENV.get_template("html.html")
 if __name__ == '__main__':
     files = (BASE_PATH/'notebooks').glob('**/*.ipynb')
     for file in files:
-        file_path = str(file).split('website-binder/notebooks/')[-1]
+        file_path = str(file).split('/notebooks/')[-1]
         output_file_path = (OUTPUT_PATH/file_path).with_suffix('.html')
         output_file_path.parent.mkdir(parents=True, exist_ok=True)
         with open(file.absolute(), encoding="utf-8") as inputf, open(output_file_path.absolute(), 'w') as outputf:
-            print(f'Converting {file.absolute()} to {output_file_path.absolute()} ...')
+            print(f'Converting {file.absolute()} to {output_file_path.absolute()}')
             nbdata = inputf.read()
             nb = nbformat.reads(nbdata, nbformat.current_nbformat)
             html_exporter = HTMLExporter(template_name='classic')
